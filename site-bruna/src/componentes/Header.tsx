7ab402a5f0f018a5ae8f87ab460af21472749c94
Gustavo-GPG <gustavogpg23@gmail.com>
2024-09-14 18:18:48 -0300

    feat: iniciando componente de cabeçalho

---
+++ site-bruna/src/componentes/Header.tsx
@@ -0,0 +1,24 @@
+function Header() {
+  return (
+    <div>
+        <a>
+            <img alt="Logo do Site" />
+        </a>
+        <nav>
+            <ul>
+                <li>
+                    <a>Home</a>
+                </li>
+                <li>
+                    <a>Sobre</a>
+                </li>
+                <li>
+                    <a>Posts</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+  );
+}
+
+export default Header;
